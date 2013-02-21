b70036e8645659e155b97b83d43d2e91bd691ef5
Victor Andrée <hej@victorandree.se>
2013-02-21 02:32:06 +0100

    HTML before JSON.

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,9 +1,9 @@
 exports.show = {
-  json: function(req, res) {
-    res.send(req.course);
-  },
   html: function(req, res) {
     res.send("course show");
+  },
+  json: function(req, res) {
+    res.send(req.course);
   }
 }
 
