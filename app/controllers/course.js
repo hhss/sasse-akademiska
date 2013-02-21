82b7c7f3853b2be343fe00645a588491efbb7b47
Victor Andrée <hej@victorandree.se>
2013-02-21 01:30:31 +0100

    Courses is becoming a nice REST endpoint.

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,3 +1,23 @@
-exports.show = function(req, res) {
-  res.send('show course');
+exports.show = {
+  json: function(req, res) {
+    res.send(req.course);
+  },
+  html: function(req, res) {
+    res.send("course show");
+  }
+}
+
+exports.load = function(req, id, fn) {
+  fn(null, {
+    id: "404",
+    name: "Microeconomics",
+    url_sse: "http://courseweb.hhs.se",
+    page: {
+      updated_at: new Date(),
+      body: "markdown body"
+    },
+    authors: [
+      { id: "hej@victorandree.se", name: "Victor Andrée", contribution: 100 }
+    ]
+  });
 }
